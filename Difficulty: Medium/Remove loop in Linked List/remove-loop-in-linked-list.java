8cd8c6084652578cb6a5736f28cdbfc10dedb64d
Manish Kumar Shukla <148890843+mkshuklaaa@users.noreply.github.com>
2025-07-05 15:15:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Remove loop in Linked List/remove-loop-in-linked-list.java
@@ -0,0 +1,31 @@
+/*
+class Node
+{
+    int data;
+    Node next;
+}
+*/
+
+class Solution {
+    // Function to remove a loop in the linked list.
+    public static void removeLoop(Node head) {
+        // code here
+        Node fast = head;
+        Node slow = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast){
+                slow = head;
+                while(slow != fast){
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                while(fast.next != slow){
+                    fast = fast.next;
+                }
+                fast.next = null;
+            }
+        }
+    }
+}
