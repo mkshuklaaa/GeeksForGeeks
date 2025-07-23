70197e85db5c502ed12bbbe62f8c63db08590a71
Manish Kumar Shukla <148890843+mkshuklaaa@users.noreply.github.com>
2025-07-23 11:07:22 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/AP Term/ap-term.py
@@ -0,0 +1,11 @@
+#User function Template for python3
+a=int(input())
+d=int(input())
+n=int(input())
+
+########### Write your code below ###############
+# Compute the AP Term
+ans =  a + (n-1) * d
+########### Write your code above ###############
+
+print(ans)
