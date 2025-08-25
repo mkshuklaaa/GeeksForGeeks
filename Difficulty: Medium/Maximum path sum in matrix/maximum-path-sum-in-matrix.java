5175230f651ac8fab639841f8f2117801cfe8f2a
Manish Kumar Shukla <148890843+mkshuklaaa@users.noreply.github.com>
2025-08-26 04:12:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Maximum path sum in matrix/maximum-path-sum-in-matrix.java
+++ Difficulty: Medium/Maximum path sum in matrix/maximum-path-sum-in-matrix.java
@@ -1,24 +1,20 @@
 // User function Template for Java
 class Solution {
-    public int max(int[][] mat,int r,int c,int[][] dp){
-        if(r>=mat.length || c>=mat[0].length) return 0;
-        if(r<0 || c<0) return 0;
-        if(dp[r][c]!=-1) return dp[r][c];
-        int dl = max(mat,r+1,c-1,dp);
-        int d = max(mat,r+1,c,dp);
-        int dr = max(mat,r+1,c+1,dp);
-        return dp[r][c]=mat[r][c] + Math.max(dl,Math.max(d,dr));
-    }
     public int maximumPath(int[][] mat) {
         // code here
         int r = mat.length;
         int c = mat[0].length;
-        int[][] dp = new int[r+1][c+1];
-        for(int i=0;i<=r;i++){
-            for(int j=0;j<=c;j++) dp[i][j]=-1;
+        int[][] dp = new int[r][c];
+        
+        for(int i=0;i<r;i++){
+            for(int j = 0;j<c;j++){
+                if(i==0) dp[i][j] = mat[i][j];
+                else dp[i][j] =mat[i][j]+Math.max((j-1>=0)?dp[i-1][j-1]:0,Math.max(dp[i-1][j],(j+1<c)?dp[i-1][j+1]:0));
+            }
         }
-        int max =0;
-        for(int i=0;i<c;i++) max = Math.max(max,max(mat,0,i,dp));
+        
+        int max = -1;
+        for(int i=0;i<c;i++) max = Math.max(max,dp[r-1][i]);
         return max;
     }
 }
