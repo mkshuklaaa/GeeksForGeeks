752f065898c78579ac54900771eeeb27bb3ec94e
Manish Kumar Shukla <148890843+mkshuklaaa@users.noreply.github.com>
2025-08-26 02:37:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Min Cost Climbing Stairs/min-cost-climbing-stairs.java
+++ Difficulty: Medium/Min Cost Climbing Stairs/min-cost-climbing-stairs.java
@@ -1,19 +1,16 @@
 //Back-end complete function Template for Java
 
 class Solution {
-    static int minClimb(int[] cost,int idx,int[] dp){
-        if(idx>=cost.length) return 0;
-        if(dp[idx]!=-1) return dp[idx];
-        int sing = cost[idx] + minClimb(cost,idx+1,dp);
-        int doub = cost[idx] + minClimb(cost,idx+2,dp);
-        int skip = minClimb(cost,idx+1,dp);
-        
-        return dp[idx] = Math.min(sing,doub);
-    }
     static int minCostClimbingStairs(int[] cost) {
         // Write your code here
-        int[] dp = new int[cost.length];
-        Arrays.fill(dp,-1);
-        return Math.min(minClimb(cost,0,dp),minClimb(cost,1,dp));
+       int n = cost.length;
+        int[] dp = new int[n];
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+
+        for(int i=2;i<n;i++){
+            dp[i] = cost[i] + Math.min(dp[i-1],dp[i-2]);
+        }
+        return Math.min(dp[n-1],dp[n-2]);
     }
 };
